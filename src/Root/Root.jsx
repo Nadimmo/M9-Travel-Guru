4e0d334c98bfff6b75f26c04d9e518f4a772351b
nadimmo <nadimmostafa334@gmail.com>
2025-01-01 23:56:48 +0600

    add login and register page

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 import Header from "../page/Header/Header";
-import Footer from "../page/Footer/Footer";
 
 
 const Root = () => {
@@ -8,7 +7,6 @@ const Root = () => {
         <div>
             <Header></Header>
             <Outlet></Outlet>
-            <Footer></Footer>
         </div>
     );
 };
--- src/page/Login/Login.jsx
+++ src/page/Login/Login.jsx
@@ -1,39 +1,72 @@
-import { NavLink } from "react-router-dom";
+/* eslint-disable react/no-unescaped-entities */
+import { FaFacebook } from "react-icons/fa"
+import { FcGoogle } from "react-icons/fc"
+import { Link } from "react-router-dom"
 
-const Login = () => {
+
+export const Login = () => {
+ 
     return (
-        <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-          </div>
-          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <form className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input type="email" placeholder="email" className="input input-bordered" required />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input type="password" placeholder="password" className="input input-bordered" required />
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                </label>
-              </div>
-              <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
-              </div>
-              <p>New to website? <NavLink to={'/register'}><a href="" className="underline">Register</a></NavLink></p>
-            </form>
-          </div>
+        <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+            <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-xl">
+                <h2 className="text-4xl text-left font-bold  text-gray-800 mb-6">
+                    Login
+                </h2>
+                <form >
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-600 mb-1">
+                            Email Address
+                        </label>
+                        <input
+                            name="email"
+                            type="email"
+                            className="w-full px-4 py-2 border  rounded-md focus:ring-2 focus:ring-[#F9A51A] focus:outline-none"
+                            placeholder="Enter your email"
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-600 mb-1">
+                            Password
+                        </label>
+                        <input
+                            name="password"
+                            type="password"
+                            className="w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-[#F9A51A] focus:outline-none"
+                            placeholder="Enter your password"
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="w-full bg-[#F9A51A] text-white py-2 px-4 rounded-md hover:bg-violet-500 transition-colors"
+                    >
+                        Sing in
+                    </button>
+                </form>
+                <p className="text-sm text-gray-600 text-center mt-6">
+                    Don't have an account?{" "}
+                    <Link to="/register" className="text-[#F9A51A] hover:underline">
+                        Sign up
+                    </Link>
+                </p>
+                <div className="flex items-center my-6">
+                    <div className="flex-grow h-px bg-gray-300"></div>
+                    <span className="mx-2 text-gray-500">or</span>
+                    <div className="flex-grow h-px bg-gray-300"></div>
+                </div>
+                {/* social login */}
+                <div>
+                    <button className="w-full py-2 px-4 border rounded-2xl flex items-center justify-center gap-2 text-gray-600 hover:bg-gray-100 transition-colors">
+                        <FcGoogle className="w-6 h-6  " />
+                        <span>Sign up with Google</span>
+                    </button>
+                    <button className="w-full mt-2 py-2 px-4 border rounded-2xl flex items-center justify-center gap-2 text-gray-600 hover:bg-gray-100 transition-colors">
+                        <FaFacebook className="w-6 h-6 text-[#3076FF]" />
+                        <span>Sign up with Facebook</span>
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-    );
-};
+    )
+}
 
-export default Login;
+export default Login
