c2a7a5e8abea398c72d133363c43eee624ac311c
nadimmo <nadimmostafa334@gmail.com>
2025-02-10 20:02:44 +0600

    get admin and dashboard route defination

---
+++ src/page/Hooks/useAdmin.jsx
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query"
+import useAxiosSecure from "./useAxiosSecure"
+import { useContext } from "react"
+import { AuthContext } from "../../AuthProvider/AuthProvider"
+
+const useAdmin = () => {
+    const axiosSecure = useAxiosSecure()
+    const { user } = useContext(AuthContext)
+
+    const { data: isAdmin } = useQuery({
+        queryKey: ["isAdmin", user?.email],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/users/admin/${user?.email}`)
+            return res.data.isAdmin
+        }
+    })
+    return { isAdmin }
+}
+
+export default useAdmin
